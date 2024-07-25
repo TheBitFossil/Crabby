53ea828f9ef890edc05e5e2cecca5c72d4d4da03
TheBitFossil <8717392-gearguard@users.noreply.gitlab.com>
2024-07-25 17:34:09 +0200

    add enemy anim source

---
+++ Source/Project/EnemyAnimationSource.h
@@ -0,0 +1,16 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "AnimSequences/Sources/PaperZDAnimationSource_Flipbook.h"
+#include "EnemyAnimationSource.generated.h"
+
+/**
+ * 
+ */
+UCLASS()
+class PROJECT_API UEnemyAnimationSource : public UPaperZDAnimationSource_Flipbook
+{
+	GENERATED_BODY()
+};
